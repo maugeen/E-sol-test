386eac80d00b53c324caef89026435347b0295f8
maugeen <101837069+maugeen@users.noreply.github.com>
2022-05-11 08:32:41 -0300

    Delete Proyect directory

--- Proyect/main2.js
+++
@@ -1,9 +0,0 @@
-let id = (id) => document.getElementById(id);
-
-let classes = (classes) => document.getElementsByClassName(classes);
-
-let primeraPregunta = id("primeraPregunta"),
-segundaPregunta = id("segundaPregunta"),
-terceraPregunta = id("terceraPregunta"),
-cuartaPregunta = id("cuartaPregunta"),
-form = id("form");

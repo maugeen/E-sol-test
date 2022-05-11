12840f48654c79ed13764629a7e26243e019ea1f
maugeen <101837069+maugeen@users.noreply.github.com>
2022-05-11 08:28:12 -0300

    Add files via upload

---
+++ Proyect/main2.js
@@ -0,0 +1,9 @@
+let id = (id) => document.getElementById(id);
+
+let classes = (classes) => document.getElementsByClassName(classes);
+
+let primeraPregunta = id("primeraPregunta"),
+segundaPregunta = id("segundaPregunta"),
+terceraPregunta = id("terceraPregunta"),
+cuartaPregunta = id("cuartaPregunta"),
+form = id("form");
